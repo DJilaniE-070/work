179b75a566eac41cd8773e473765f8ecd951022e
root <root@Debian-DJE.DJE>
2018-02-18 23:57:48 +0100

    signed apk added

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,30 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-firebase.FirebasePlugin",
+    "file": "plugins/cordova-plugin-firebase/www/firebase.js",
+    "pluginId": "cordova-plugin-firebase",
+    "clobbers": [
+      "FirebasePlugin"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-android-support-gradle-release": "1.2.0",
+  "cordova-plugin-firebase": "0.1.25",
+  "cordova-plugin-inappbrowser": "2.0.1",
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
